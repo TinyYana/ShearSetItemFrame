f9a26e065c04c10d1d67c7cf5468ac38c1816ad9
夜花櫻 <yanasakuranight@gmail.com>
2024-06-09 21:03:44 +0800

    Initial commit

---
+++ src/main/java/tinyyana/shearsetitemframe/SetItemFrame.java
@@ -0,0 +1,2 @@
+package tinyyana.shearsetitemframe;public class SetItemFrame {
+}
---
+++ src/main/java/tinyyana/shearsetitemframe/ShearSetItemFramePlugin.java
@@ -0,0 +1,17 @@
+package tinyyana.shearsetitemframe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShearSetItemFramePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
